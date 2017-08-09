f60b7d58858a15a9c8db9d39366ea84f185ad3d4
junnnnn06 <JUN@adachijun-no-MacBook-Air-2.local>
2017-08-09 16:30:43 +0900

    sign up update

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,11 +3,14 @@ class LikesController < ApplicationController
   def create
     @like = Like.create(user_id: current_user.id, item_id: params[:tweet_id])
     @likes = Like.where(item_id: params[:item_id])
+    @items = Item.find(params[:id])
   end
 
   def destroy
     like = Like.find_by(user_id: current_user.id, item_id: params[:id])
     like.destroy
     @likes = Like.where(item_id: params[:item_id])
+    @items = Item.find(params[:id])
   end
+
 end
