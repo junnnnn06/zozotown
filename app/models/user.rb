63aeb4f0029f7cb195729b129ceed9feecf2fdfa
junnnnn06 <JUN@adachijun-no-MacBook-Air-2.local>
2017-08-12 18:03:20 +0900

    add validate

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,5 @@ class User < ApplicationRecord
   validates :encrypted_password, presence: true, uniqueness: true,
              length: {minimum: 8}
   validates :email, presence: true, uniqueness: true
+  validates :name, presence: true
 end
