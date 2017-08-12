4ed845444396bad1ed39c82ed02e120cf622fb63
junnnnn06 <JUN@adachijun-no-MacBook-Air-2.local>
2017-08-12 17:01:23 +0900

    削除　バリデーション　名前

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,5 +10,4 @@ class User < ApplicationRecord
   validates :encrypted_password, presence: true, uniqueness: true,
              length: {minimum: 8}
   validates :email, presence: true, uniqueness: true
-  validates :name, presence: true
 end
