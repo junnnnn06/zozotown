3372dab469e933c60d89e7c3e93c9c635506c709
junnnnn06 <junnnnn06@icloud.com>
2017-08-19 13:50:08 +0900

    mailer prooduction change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,15 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    domain:               'smtp.gmail.com',
+    user_name:            ENV['GMAIL_USER_NAME'],
+    password:             ENV['GMAIL_PASSWORD'],
+    authentication:       'login',
+    enable_starttls_auto: true
+  }
+  config.action_mailer.default_url_options = { host: '54.64.190.132' }
+
 end
