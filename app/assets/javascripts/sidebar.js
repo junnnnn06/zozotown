35772a1df8b3f8a9ffc0ba846eb144d3c2cfcfff
junnnnn06 <junnnnn06@icloud.com>
2017-08-28 16:56:07 +0900

    ボタンのカラー修正

--- app/assets/javascripts/sidebar.js
+++ app/assets/javascripts/sidebar.js
@@ -5,3 +5,21 @@ $(function() {
      $(this).css('background', '');
   });
 });
+
+$(function() {
+  $('li.drawer-dropdown').hover(function() {
+    $(this).css('background', '#333333');
+  }, function() {
+     $(this).css('background', '');
+  });
+  $('a.drawer-menu-item').hover(function() {
+    $(this).css('background', '#333333');
+  }, function() {
+     $(this).css('background', '');
+  });
+  $('a.drawer-dropdown-menu-item').hover(function() {
+    $(this).css('background', 'white');
+  }, function() {
+     $(this).css('background', '');
+  });
+});
